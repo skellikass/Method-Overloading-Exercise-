6be55ca8bb5ef4a9878e45495dcebaaf34fa8f87
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-02 12:16:01 -0500

    completed exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace MethodOverloading
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Add (int int1, int int2)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(int1 + int2);
+        }
+        public static void Add(decimal dec1, decimal dec2)
+        {
+            Console.WriteLine(dec1 + dec2);
+        }
+        public static string Add(int int1, int int2, bool bool1)
+        {
+            if (bool1 == true && int1 + int2 > 1 || int1 + int2 < 1)
+            {
+                return $"{int1 + int2} dollars";
+            }
+            else 
+            {
+                return $"{int1 + int2} dollar";
+            }
+        }
+        static void Main()
+        {
+            Add(12, 3);
+            Add(1.23m, 4.56m);
+            Console.WriteLine(Add(1, 23, true));
         }
     }
 }
